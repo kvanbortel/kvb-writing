6cb0d9adebbfb8e5036a275075bb120f7ed10768
kvanbortel <102192092+kvanbortel@users.noreply.github.com>
2025-03-09 15:19:05 -0400

    set proper slug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/nextjs-github-pages/next.svg"
+          src="/kvb-writing/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -32,7 +32,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/nextjs-github-pages/vercel.svg"
+              src="/kvb-writing/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -58,7 +58,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/nextjs-github-pages/file.svg"
+            src="/kvb-writing/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -73,7 +73,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/nextjs-github-pages/window.svg"
+            src="/kvb-writing/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -88,7 +88,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/nextjs-github-pages/globe.svg"
+            src="/kvb-writing/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
