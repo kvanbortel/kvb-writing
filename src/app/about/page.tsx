45cca12b2609003d7793672b3305afd74e97bad8
kvanbortel <102192092+kvanbortel@users.noreply.github.com>
2025-03-12 17:28:35 -0400

    rewrite about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -27,19 +27,21 @@ export default function AboutPage() {
       <h1 className="text-4xl font-bold mb-8">About Me</h1>
       <div className="grid md:grid-cols-[2fr_1fr] gap-12">
         <article className="space-y-6">
-          <p className="text-gray-600 dark:text-gray-300 leading-relaxed">
-            Hi, I&apos;m Kayla Van Bortel! I&apos;m a lifelong storyteller from upstate New York where I&apos;m 
-            currently attending Rochester Institute of Technology.
-            I have been writing and researching the craft for years and now I&apos;m pursuing a Creative Writing minor
-            and hunting down any writing groups I can. I&apos;m dedicated to 
-            crafting compelling narratives that resonate with readers and transport them to worlds as lively as the
-            ones that live in my mind.
+          <p className="text-gray-600 dark:text-gray-300 leading-relaxed text-justify">
+            Hi, I&apos;m Kayla Van Bortel! I&apos;m a lifelong storyteller from upstate New York, where I&apos;m
+            pursuing a Creative Writing minor and engaging with writing communities whereever I go.
           </p>
-          <p className="text-gray-600 dark:text-gray-300 leading-relaxed">
-            When I&apos;m not writing, you&apos;ll probably find me exploring local bookstores, camping in the
-            Adirondacks, skiing (on snow or water, depending on the season), playing soccer, or sitting in front of a
-            glowing screen, a blanket wrapped around my shoulders, typing into terminal windows and my IDE of choice.
-            I believe every experience feeds into the stories we tell—and what a freeing rule that is to live by.
+          <p className="text-gray-600 dark:text-gray-300 leading-relaxed text-justify">
+            I&apos;m dedicated to crafting immersive narratives with dynamic characters and explorations
+            of the gripping questions that unfold. Though I&apos;m primarily a fantasy writer, I love
+            weaving elements of horror, suspense, and gothic fiction into my stories, balancing eerie atmospheres with
+            softer moments.
+          </p>
+          <p className="text-gray-600 dark:text-gray-300 leading-relaxed text-justify">
+            When I&apos;m not writing, you can find me exploring bookstores, camping in the
+            Adirondacks, skiing (on snow or water, depending on the season), or sitting in front of a glowing screen,
+            a blanket wrapped around my shoulders, programming in my IDE of choice. I believe every
+            experience feeds into the stories we tell, and embracing that idea makes life all the more exciting.
           </p>
         </article>
         <figure className="relative aspect-[2/3] rounded-lg overflow-hidden bg-gray-100 dark:bg-gray-800">
